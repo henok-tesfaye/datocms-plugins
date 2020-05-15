c4b970b3a132b6949e814e49b143933797c811c2
Henok Tesfaye <henoktesfaye72@gmail.com>
2020-05-15 13:48:44 +0300

    feat: setup react app rewired and ckeditor-mathtype

---
+++ src/plugins/ckeditor-mathtype/Main.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+import CKEditor from '@ckeditor/ckeditor5-react'
+import ClassicEditor from './ckeditor5/build/ckeditor'
+
+import connectToDatoCms from '../connectToDatoCms'
+import './style.css'
+
+class Main extends Component {
+  render () {
+    const { plugin, fieldValue } = this.props
+    return (
+      <CKEditor
+        editor={ClassicEditor}
+        data={fieldValue}
+        onInit={(editor) => {
+          // You can store the "editor" and use when it is needed.
+          console.log('Editor is ready to use!', editor)
+        }}
+        onChange={(event, editor) => {
+          const data = editor.getData()
+          plugin.setFieldValue(plugin.fieldPath, data)
+        }}
+      />
+    )
+  }
+}
+
+const mapPluginToProps = plugin => ({
+  developmentMode: plugin.parameters.global.developmentMode,
+  fieldValue: plugin.getFieldValue(plugin.fieldPath)
+})
+
+export default connectToDatoCms(mapPluginToProps)(Main)
